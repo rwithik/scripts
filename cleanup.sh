0e90469f6dd2715970d6d026f7b23cca4e9079de
rwithik <rwithik@gmail.com>
2019-06-19 22:26:14 +0530

    Initial Commit

---
+++ cleanup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [[ ! -d PDFs ]]; then
+	mkdir PDFs
+fi
+if [[ ! -d Images ]]; then
+	mkdir Images
+fi
+if [[ ! -d GIFs ]]; then
+	mkdir GIFs
+fi
+if [[ ! -d Audios ]]; then
+	mkdir Audios
+fi
+if [[ ! -d Archives ]]; then
+	mkdir Archives
+fi
+if [[ ! -d Docs ]]; then
+	mkdir Docs
+fi
+if [[ ! -d E-Books ]]; then
+	mkdir E-Books
+fi
+
+mv -v ./*.pdf ./PDFs/ 2> /dev/null
+mv -v ./*.jpg ./*.jpeg ./*.png ./Images/ 2> /dev/null
+mv -v ./*.gif ./GIFs/ 2> /dev/null
+mv -v ./*.tar.bz2 ./*.tar.gz ./*.bz2 ./*.rar ./*.tbz2 ./*.gz ./*.tgz ./*.zip ./*.Z ./*.7z Archives/ 2> /dev/null
+mv -v ./*.mp4 ./*.amr ./Audios/ 2> /dev/null
+mv -v ./*.docx ./*.doc ./*.odt ./*.ppt ./Docs/ 2> /dev/null
+mv -v ./*.mobi ./*.epub ./E-Books/ 2> /dev/null
---
+++ wallpaperScript.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+wal -i ~/wallpapers/Landscapes >> /dev/null
+
+source ~/.cache/wal/colors.sh
